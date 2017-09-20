84c4013da37258d0e43906033b94bd0f55437d83
Abdul Samad <sammu899@gmail.com>
2017-09-20 05:44:34 +0500

    created repo , Added first.py

---
+++ first.py
@@ -0,0 +1,12 @@
+animals = ['cat', 'dog', 'monkey']
+for idx, animal in enumerate(animals):
+    print('#%d: %s' % (idx + 1, animal))
+print ("--------------------------------------------------")
+
+
+i=[1,2,3,4,5,6,7,8]
+square=[]
+
+square=[a**2 for a in i]
+
+print (square)
