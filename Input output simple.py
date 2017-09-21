dc11777794a64909ef4c2101a8f07e734ff0fb90
Abdul Samad <sammu899@gmail.com>
2017-09-21 07:55:30 +0500

    Loops

--- Input output simple.py
+++ Input output simple.py
@@ -1,4 +1,12 @@
-Name= input("May I know Your Name Sire")
+Name= input("May I know Your Name Sire \n")
 print("Welcome Mr",Name)
-Age= input("What is your age???")
-print("You look too young to be " + Age)
+Age= int(input("What is your age???"))
+
+if Age>=21:
+    print(Name,"You are allowed in the Club")
+    print("What drink do u want???")
+elif Age>=18:
+    print("You are allowed in Club but u cant have drinks",end='\n''\n')
+
+else:
+    print(Name,"Unfortuntely You are not allowed  in the Club")
