6d24e0f1c149c4082718dae8394bab508e10d2ab
Abdul Samad <sammu899@gmail.com>
2017-09-21 07:24:11 +0500

    input output program

---
+++ Input output simple.py
@@ -0,0 +1,4 @@
+Name= input("May I know Your Name Sire")
+print("Welcome Mr",Name)
+Age= input("What is your age???")
+print("You look too young to be " + Age)
--- first.py
+++ first.py
@@ -1,7 +1,7 @@
 animals = ['cat', 'dog', 'monkey']
 for idx, animal in enumerate(animals):
     print('#%d: %s' % (idx + 1, animal))
-print ("--------------------------------------------------")
+#----------------------------------------
 
 
 i=[1,2,3,4,5,6,7,8]
@@ -10,3 +10,4 @@ square=[]
 square=[a**2 for a in i]
 
 print (square)
+#---------------------------------------------------
